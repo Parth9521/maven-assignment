e550065fc53869bdb59f643fbaf7d860fca9e778
Parth9521 <parth.dhiman@knoldus.com>
2021-08-03 23:04:43 +0530

    Initial commit

---
+++ Calapi/src/main/java/Calulator.java
@@ -0,0 +1,43 @@
+public class Calulator {
+
+    /**
+     * @param num1
+     * @param num2
+     * @return it will return addition
+     */
+    public int add(final int num1, final int num2) {
+        int result = num1 + num2;
+        return result;
+    }
+
+    /**
+     * @param num1
+     * @param num2
+     * @return it will return difference
+     */
+    public int substract(final int num1, final int num2) {
+        int result = num1 - num2;
+        return result;
+    }
+
+    /**
+     * @param num1
+     * @param num2
+     * @return it will return multiply
+     */
+    public int multiply(final int num1, final int num2) {
+        int result = num1 * num2;
+        return result;
+    }
+
+    /**
+     * @param num1
+     * @param num2
+     * @return it will return division
+     */
+    public double divide(final double num1, final double num2) {
+         double result = num1 / num2;
+        return result;
+    }
+
+}
---
+++ calimpl/src/main/java/Application.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public final class Application {
+    private Application() {
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enetr 2 digits ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println("make your selection ");
+        System.out.println("ADD ");
+        System.out.println("SUB ");
+        System.out.println("MULTI ");
+        System.out.println("DIV ");
+        int select = scan.nextInt();
+        final int max = 4;
+        final int multi = 3;
+        while (select > max) {
+            System.out.println("wrong choice ");
+            select = scan.nextInt();
+        }
+        Calulator mani = new Calulator();
+        if (select == 1) {
+            int c = mani.add(a, b);
+        } else if (select == 2) {
+            int c = mani.substract(a, b);
+        } else if (select == multi) {
+            int c = mani.multiply(a, b);
+        } else {
+            double c = mani.divide(a, b);
+        }
+
+    }
+}
